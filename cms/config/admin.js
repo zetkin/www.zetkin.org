fba2587f8a3ee2b48c27aefb654fa431e5bfed74
Richard Olsson <r@richardolsson.se>
2022-01-24 08:15:09 +0100

    Set up NEXT.js and Strapi boilerplates

---
+++ cms/config/admin.js
@@ -0,0 +1,5 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'e7572fecd66d651427d24c3917ca7135'),
+  },
+});
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Zetkin Foundation</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          This will be the new website of Zetkin Foundation.
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
