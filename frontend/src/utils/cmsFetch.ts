49a1736ec476feecf85ddc492453febef0fa33f7
Richard Olsson <r@richardolsson.se>
2022-01-31 10:12:47 +0100

    Configure CMS host using environment variable

--- frontend/src/utils/cmsFetch.ts
+++ frontend/src/utils/cmsFetch.ts
@@ -1,4 +1,6 @@
+const CMS_HOST = process.env.CMS_HOST || 'localhost:1337';
+
 export default async function cmsFetch(apiPath: string) {
-  const url = `http://cms:1337/api${apiPath}`;
+  const url = `http://${CMS_HOST}/api${apiPath}`;
   return fetch(url);
 }
