029d26d34c86583e83df04c5951396001d4e1735
ziggi <ziggi.willpower@gmail.com>
2022-02-08 12:02:27 +0100

    populates main-menu when fetched from cms

--- cms/src/api/main-menu/controllers/main-menu.js
+++ cms/src/api/main-menu/controllers/main-menu.js
@@ -6,4 +6,31 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::main-menu.main-menu');
+module.exports = createCoreController('api::main-menu.main-menu', ({ strapi }) => ({
+  async find(ctx) {
+    const entity = await strapi.entityService.findMany('api::main-menu.main-menu', {
+      fields: ['*'],
+      populate: {
+        section1: {
+          populate: {
+            cta: {
+              populate: {
+                page: true,
+              }
+            },
+            image: true,
+            links: {
+              populate: {
+                page: true,
+              }
+            },
+          }
+        },
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+
+  }
+}));
