714d99415d81d866a927ee7d14fd5f964f0db658
ziggi <ziggi.willpower@gmail.com>
2022-02-18 10:57:27 +0100

    refactor with Prettier formatting

--- frontend/src/components/LogoWall.tsx
+++ frontend/src/components/LogoWall.tsx
@@ -1,43 +1,48 @@
-import { Link } from "@mui/material";
-import { Box } from "@mui/system";
-import Image from "next/image";
-import NextLink from "next/link";
-import { CmsLogo } from "../types/cms";
-import getCmsMedia from "../utils/getCmsMedia";
-import makePageUrl from "../utils/makePageUrl";
+import { Link } from '@mui/material';
+import { Box } from '@mui/system';
+import Image from 'next/image';
+import NextLink from 'next/link';
+import { CmsLogo } from '../types/cms';
+import getCmsMedia from '../utils/getCmsMedia';
+import makePageUrl from '../utils/makePageUrl';
 
 const LogoWall = ({ ...rest }) => {
   const { logos } = rest;
 
   return (
     <Box>
-      {
-        logos.map((logo: CmsLogo, index: number) => {
-          return logo.externalUrl || logo.page ?
-            <NextLink
-              href={logo.externalUrl? logo.externalUrl : makePageUrl(logo.page!.data?.id)}
-              key={`logo-${index}`}
-              passHref
-            >
-              <Link>
-                <Image
-                  alt={`Go to ${logo.externalUrl}`}
-                  height={100}
-                  src={getCmsMedia(logo.image.data.attributes.formats.thumbnail.url)}
-                  width={100}
-                />
-              </Link>
-            </NextLink>
-            :
-            <Image
-              alt={logo.image.data.attributes.alternativeText}
-              height={100}
-              key={`logo-${index}`}
-              src={getCmsMedia(logo.image.data.attributes.formats.thumbnail.url)}
-              width={100}
-            />
-        })
-      }
+      {logos.map((logo: CmsLogo, index: number) => {
+        return logo.externalUrl || logo.page ? (
+          <NextLink
+            href={
+              logo.externalUrl
+                ? logo.externalUrl
+                : makePageUrl(logo.page!.data?.id)
+            }
+            key={`logo-${index}`}
+            passHref
+          >
+            <Link>
+              <Image
+                alt={`Go to ${logo.externalUrl}`}
+                height={100}
+                src={getCmsMedia(
+                  logo.image.data.attributes.formats.thumbnail.url
+                )}
+                width={100}
+              />
+            </Link>
+          </NextLink>
+        ) : (
+          <Image
+            alt={logo.image.data.attributes.alternativeText}
+            height={100}
+            key={`logo-${index}`}
+            src={getCmsMedia(logo.image.data.attributes.formats.thumbnail.url)}
+            width={100}
+          />
+        );
+      })}
     </Box>
   );
 };
